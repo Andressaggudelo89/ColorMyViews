0d2ada97fbfe595a7831169b8bb3af3210328a5e
andressaggududelo89 <cagudeloa@uninorte.edu.co>
2019-12-02 12:50:19 -0500

    More colors and buttons added

--- app/src/main/java/com/andressaggudelo89/colormyviews/MainActivity.kt
+++ app/src/main/java/com/andressaggudelo89/colormyviews/MainActivity.kt
@@ -11,16 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setListeners()
-
     }
 
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(
                 box_one_text, box_two_text, box_three_text,
-                box_four_text,box_five_text, constraint_layout
+                box_four_text,box_five_text, constraint_layout,
+                red_button, yellow_button, green_button
             )
         for (item in clickableViews){
             item.setOnClickListener {
@@ -28,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     fun makeColored(view: View) {
         when (view.id) {
 
@@ -40,7 +40,12 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.CYAN)
         }
     }
 }
